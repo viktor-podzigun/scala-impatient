49f4f7f62350eec86d026d6b949f7a91e3a0cce6
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-01-18 10:03:40 +0100

    Added solution for task 5.1

--- src/main/scala/Chapter05.scala
+++ src/main/scala/Chapter05.scala
@@ -4,8 +4,20 @@ object Chapter05 {
 
   /**
    * Task 1:
-   *
+   *  Improve the Counter class in Section 5.1, "Simple Classes and Parameterless Methods"
+   *  on page 51 so that it doesn't turn negative at Int.MaxValue
    */
-  def task01() = {
+  class Counter01(private var value: Int = 0) {
+
+    def increment(): Int = {
+      if (value == Int.MaxValue) {
+        throw new IllegalStateException("counter reached Int.MaxValue")
+      }
+
+      value += 1
+      value
+    }
+
+    def current: Int = value
   }
 }
--- src/test/scala/Chapter05Spec.scala
+++ src/test/scala/Chapter05Spec.scala
@@ -3,6 +3,15 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Chapter05Spec extends FlatSpec with Matchers {
 
-  "Chapter05" should "test task 01" in {
+  "Chapter05.Counter01" should "not turn negative at Int.MaxValue" in {
+    //when
+    val counter = new Counter01(Int.MaxValue - 1)
+
+    //then
+    counter.current shouldBe (Int.MaxValue - 1)
+    counter.increment() shouldBe Int.MaxValue
+    a [IllegalStateException] should be thrownBy {
+      counter.increment()
+    }
   }
 }
