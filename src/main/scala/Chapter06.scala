ceaa740955777fe1e5d9d4a0da3d9558885a189a
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-01-22 10:28:26 +0100

    Added solution for task 6.2

--- src/main/scala/Chapter06.scala
+++ src/main/scala/Chapter06.scala
@@ -14,4 +14,30 @@ object Chapter06 {
 
     def milesToKilometers(miles: Double): Double = miles / 0.62137
   }
+
+  /**
+   * Task 2:
+   *  The preceding problem wasn't very object-oriented. Provide a general super-class
+   *  UnitConversion and define objects InchesToCentimeters, GallonsToLiters, and
+   *  MilesToKilometers that extend it.
+   */
+  abstract class UnitConversion {
+
+    def convert(inches: Double): Double
+  }
+
+  object InchesToCentimeters extends UnitConversion {
+
+    override def convert(inches: Double) = inches * 2.54
+  }
+
+  object GallonsToLiters extends UnitConversion {
+
+    override def convert(gallons: Double) = gallons * 3.785
+  }
+
+  object MilesToKilometers extends UnitConversion {
+
+    override def convert(miles: Double) = miles / 0.62137
+  }
 }
--- src/test/scala/Chapter06Spec.scala
+++ src/test/scala/Chapter06Spec.scala
@@ -3,9 +3,20 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Chapter06Spec extends FlatSpec with Matchers {
 
-  "Conversions" should "not turn negative at Int.MaxValue" in {
+  "Conversions" should "convert units using separate methods" in {
     Conversions.inchesToCentimeters(1).formatted("%.2f") shouldBe "2.54"
     Conversions.gallonsToLiters(1).formatted("%.3f") shouldBe "3.785"
     Conversions.milesToKilometers(1).formatted("%.6f") shouldBe "1.609347"
+  }
+
+  "UnitConversion" should "convert units using general super-class" in {
+    val inchesToCentimeters: UnitConversion = InchesToCentimeters
+    inchesToCentimeters.convert(1).formatted("%.2f") shouldBe "2.54"
+
+    val gallonsToLiters: UnitConversion = GallonsToLiters
+    gallonsToLiters.convert(1).formatted("%.3f") shouldBe "3.785"
+
+    val milesToKilometers: UnitConversion = MilesToKilometers
+    milesToKilometers.convert(1).formatted("%.6f") shouldBe "1.609347"
   }
 }
