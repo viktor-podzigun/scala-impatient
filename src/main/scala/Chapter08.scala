98e0fe6177344390baff306a0bff390933802028
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-01-28 11:41:03 +0100

    Added solution for task 8.2

--- src/main/scala/Chapter08.scala
+++ src/main/scala/Chapter08.scala
@@ -34,3 +34,49 @@ class CheckingAccount(initialBalance: Double) extends BankAccount(initialBalance
 
   override def withdraw(amount: Double) = super.withdraw(amount + charge)
 }
+
+/**
+ * Task 2:
+ *
+ * Extend the `BankAccount` class of the preceding exercise into a class `SavingsAccount`
+ * that earns interest every month (when a method `earnMonthlyInterest` is called)
+ * and has three free deposits or withdrawals every month. Reset the transaction
+ * count in the `earnMonthlyInterest` method.
+ */
+class SavingsAccount(initialBalance: Double) extends BankAccount(initialBalance) {
+
+  private val monthlyInterest: Double = 0.01
+  private val maxFreeTransactions: Int = 3
+
+  private var transactionsCount: Int = 0
+  private var balance: Double = initialBalance
+
+  override def deposit(amount: Double) = {
+    balance = super.deposit(amount - charge)
+    balance
+  }
+
+  override def withdraw(amount: Double) = {
+    balance = super.withdraw(amount + charge)
+    balance
+  }
+
+  def getBalance: Double = {
+    balance
+  }
+
+  def earnMonthlyInterest(): Unit = {
+    transactionsCount = 0
+
+    balance = super.deposit(balance * monthlyInterest)
+  }
+
+  private def charge: Double = {
+    if (transactionsCount < maxFreeTransactions) {
+      transactionsCount += 1
+      return 0.0
+    }
+
+    1.0
+  }
+}
--- src/test/scala/Chapter08Spec.scala
+++ src/test/scala/Chapter08Spec.scala
@@ -7,4 +7,16 @@ class Chapter08Spec extends FlatSpec with Matchers {
     account.deposit(5) shouldBe 104
     account.withdraw(5) shouldBe 98
   }
+
+  "SavingsAccount" should "earn interest every month" in {
+    val account = new SavingsAccount(100)
+    account.deposit(5) shouldBe 105
+    account.withdraw(5) shouldBe 100
+    account.deposit(3) shouldBe 103
+    account.withdraw(3) shouldBe 99
+    account.deposit(2) shouldBe 100
+    account.earnMonthlyInterest()
+    account.getBalance shouldBe 101
+    account.withdraw(5) shouldBe 96
+  }
 }
