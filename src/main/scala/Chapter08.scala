b3804ffab8d34dacabcf4843a78782fc123c3f57
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-01-29 15:10:38 +0100

    Added solution for task 8.3

--- src/main/scala/Chapter08.scala
+++ src/main/scala/Chapter08.scala
@@ -80,3 +80,48 @@ class SavingsAccount(initialBalance: Double) extends BankAccount(initialBalance)
     1.0
   }
 }
+
+/**
+ * Task 3:
+ *
+ * Consult your favorite Java or C++ textbook that is sure to have an example
+ * of a toy inheritance hierarchy, perhaps involving employees, pets, graphical
+ * shapes, or the like. Implement the example in Scala.
+ */
+abstract class Shape {
+  def draw(): Unit
+  def erase(): Unit
+}
+
+class Circle extends Shape {
+
+  override def draw(): Unit = {
+    println("drawing Circle...")
+  }
+
+  override def erase(): Unit = {
+    println("erasing Circle...")
+  }
+}
+
+class Square extends Shape {
+
+  override def draw(): Unit = {
+    println("drawing Square...")
+  }
+
+  override def erase(): Unit = {
+    println("erasing Square...")
+  }
+}
+
+object Shapes extends App {
+
+  cleanAndPaint(new Circle)
+  cleanAndPaint(new Square)
+
+  def cleanAndPaint(shape: Shape): Unit = {
+    shape.erase()
+    shape.draw()
+  }
+}
