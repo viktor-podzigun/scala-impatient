c92255154648f833b6981b861316086bf03af990
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-02-04 10:54:08 +0100

    Added solution for task 8.9

--- src/main/scala/Chapter08.scala
+++ src/main/scala/Chapter08.scala
@@ -266,3 +266,36 @@ class SecretAgent(codename: String) extends Person(codename) {
 }
 
 }
+
+/**
+ * Task 9:
+ *
+ * In the `Creature` class of Section 8.10, "Construction Order and Early Definitions,"
+ * on page 94, replace `val range` with a `def`. What happens when you also use a
+ * `def` in the `Ant` subclass? What happens when you use a `val` in the subclass?
+ * Why?
+ */
+class Creature {
+  def range: Int = 10
+  val env: Array[Int] = new Array[Int](range)
+}
+
+class Ant extends Creature {
+  override def range = 2
+}
+
+/**
+ * Solution:
+ *
+ * When we use def in both superclass and subclass the array length is initialized with expected,
+ * overrided value 2. This happens because there is no field to hold the value, and we don't have
+ * initialization order problem.
+ *
+ * But when we use def in superclass and val in subclass initialization order problem is back
+ * again. Since now the range value in subclass is backed by field and at the moment when
+ * corresponding generated range getter method is called from superclass constructor the value is
+ * not initialized yet and equals to 0 by default.
+ */
+object Creatures extends App {
+  println(new Ant().env.length)
+}
