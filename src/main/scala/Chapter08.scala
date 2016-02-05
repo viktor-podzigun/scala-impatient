b7edb45da44b073d0a81f80d7f6fa7963a0df2f9
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-02-05 11:19:45 +0100

    Added solution for task 8.10

--- src/main/scala/Chapter08.scala
+++ src/main/scala/Chapter08.scala
@@ -299,3 +299,22 @@ class Ant extends Creature {
 object Creatures extends App {
   println(new Ant().env.length)
 }
+
+/**
+ * Task 10:
+ *
+ * The file `scala/collection/immutable/Stack.scala` contains the definition
+ * {{{
+ * class Stack[A] protected (protected val elems: List[A])
+ * }}}
+ * Explain the meanings of the `protected` keywords. (Hint: Review the discussion
+ * of private constructors in Chapter 5.)
+ *
+ *
+ * Solution:
+ *
+ * The first `protected` keyword defines protected primary constructor, which is accessible only
+ * from auxiliary constructor or from subclass primary constructor.
+ * The second `protected` keyword defines protected field `elems` with corresponding protected
+ * getter method, they are accessible within class and subclasses.
+ */
