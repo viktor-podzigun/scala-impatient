85e684633c3fd837593654b1f2a39e6773a251f3
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-02-21 13:01:31 +0100

    Added solution for task 10.5

--- src/main/scala/Chapter10.scala
+++ src/main/scala/Chapter10.scala
@@ -1,4 +1,5 @@
 import java.awt.Point
+import java.beans.{PropertyChangeEvent, PropertyChangeListener, PropertyChangeSupport}
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
@@ -290,4 +291,62 @@ object Chapter10 {
       super.log(msg.map(c => (c + key).toChar))
     }
   }
+
+  /**
+   * Task 5:
+   *
+   * The JavaBeans specification has the notion of a property change listener, a standardized
+   * way for beans to communicate changes in their properties. The `PropertyChangeSupport` class
+   * is provided as a convenience superclass for any bean that wishes to support property change
+   * listeners. Unfortunately, a class that already has another superclass - such as JComponent -
+   * must reimplement the methods. Reimplement `PropertyChangeSupport` as a trait,
+   * and mix it into the `java.awt.Point` class.
+   */
+  trait PropertyChangeSupportLike {
+    private val support = new PropertyChangeSupport(this)
+
+    def addPropertyChangeListener(listener: PropertyChangeListener): Unit =
+      support.addPropertyChangeListener(listener)
+
+    def removePropertyChangeListener(listener: PropertyChangeListener): Unit =
+      support.removePropertyChangeListener(listener)
+
+    def getPropertyChangeListeners: Array[PropertyChangeListener] =
+      support.getPropertyChangeListeners
+
+    def addPropertyChangeListener(propertyName: String, listener: PropertyChangeListener): Unit =
+      support.addPropertyChangeListener(propertyName, listener)
+
+    def removePropertyChangeListener(propertyName: String, listener: PropertyChangeListener): Unit =
+      support.removePropertyChangeListener(propertyName, listener)
+
+    def getPropertyChangeListeners(propertyName: String): Array[PropertyChangeListener] =
+      support.getPropertyChangeListeners(propertyName)
+
+    def firePropertyChange(propertyName: String, oldValue: Any, newValue: Any): Unit =
+      support.firePropertyChange(propertyName, oldValue, newValue)
+
+    def firePropertyChange(propertyName: String, oldValue: Int, newValue: Int): Unit =
+      support.firePropertyChange(propertyName, oldValue, newValue)
+
+    def firePropertyChange(propertyName: String, oldValue: Boolean, newValue: Boolean): Unit =
+      support.firePropertyChange(propertyName, oldValue, newValue)
+
+    def firePropertyChange(event: PropertyChangeEvent): Unit =
+      support.firePropertyChange(event)
+
+    def fireIndexedPropertyChange(propertyName: String, index: Int, oldValue: Any, newValue: Any): Unit =
+      support.fireIndexedPropertyChange(propertyName, index, oldValue, newValue)
+
+    def fireIndexedPropertyChange(propertyName: String, index: Int, oldValue: Int, newValue: Int): Unit =
+      support.fireIndexedPropertyChange(propertyName, index, oldValue, newValue)
+
+    def fireIndexedPropertyChange(propertyName: String, index: Int, oldValue: Boolean, newValue: Boolean): Unit =
+      support.fireIndexedPropertyChange(propertyName, index, oldValue, newValue)
+
+    def hasListeners(propertyName: String): Boolean =
+      support.hasListeners(propertyName)
+  }
+
+  class PointBean(x: Int = 0, y: Int = 0) extends Point(x, y) with PropertyChangeSupportLike
 }
