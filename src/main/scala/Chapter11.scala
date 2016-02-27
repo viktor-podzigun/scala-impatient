2dad987afa1404b251c28c9f36ae12e8b2d6c18c
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-02-27 13:44:27 +0100

    Added solution for task 11.2

--- src/main/scala/Chapter11.scala
+++ src/main/scala/Chapter11.scala
@@ -13,4 +13,19 @@ object Chapter11 {
    * `3 + 4 -> 5` evaluated as `(3 + 4) -> 5` and
    * `3 -> 4 + 5` evaluated as `(3 -> 4) + 5`
    */
+
+  /**
+   * Task 2:
+   *
+   * The `BigInt` class has a `pow` method, not an operator.
+   * Why didn't the Scala library designers choose `**` (as in Fortran) or `^` (as in Pascal)
+   * for a power operator?
+   *
+   * Solution:
+   *
+   * First, the precedence is determined by the first character, the `**` operator would has
+   * the same precedence as `*`, `/` operators, but `pow` operation should have higher precedence.
+   * Second, the `^` operator is already exists for `BigInt` class, which is
+   * "Bitwise exclusive-or of BigInts".
+   */
 }
