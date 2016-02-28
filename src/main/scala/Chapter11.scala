7f69598a2340f35eaeb36c201a5d81c485d6b089
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-02-28 20:57:19 +0100

    Prepared solution for task 11.3

--- src/main/scala/Chapter11.scala
+++ src/main/scala/Chapter11.scala
@@ -28,4 +28,21 @@ object Chapter11 {
    * Second, the `^` operator is already exists for `BigInt` class, which is
    * "Bitwise exclusive-or of BigInts".
    */
+
+  /**
+   * Task 3:
+   *
+   * Implement the `Fraction` class with operations `+`, `-`, `*`, `/`. Normalize fractions,
+   * for example turning `15/-6` into `-5/2`. Divide by the greatest common divisor, like this:
+   * {{{
+   *  class Fraction(n: Int, d: Int) {
+   *    private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d)
+   *    private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d)
+   *    override def toString = num + “/” + den
+   *    def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
+   *    def gcd(a: Int, b: Int): Int = if (b == 0) abs(a) else gcd(b, a % b)
+   *    ...
+   *  }
+   * }}}
+   */
 }
