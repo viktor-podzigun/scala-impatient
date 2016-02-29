2dc392d60111995a746cd0ae919cfd5d6bcb007a
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-02-29 10:00:12 +0100

    Added solution for task 11.3

--- src/main/scala/Chapter11.scala
+++ src/main/scala/Chapter11.scala
@@ -1,4 +1,4 @@
-
+import scala.annotation.tailrec
 
 object Chapter11 {
 
@@ -38,11 +38,46 @@ object Chapter11 {
    *  class Fraction(n: Int, d: Int) {
    *    private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d)
    *    private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d)
-   *    override def toString = num + “/” + den
+   *    override def toString = num + "/" + den
    *    def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
    *    def gcd(a: Int, b: Int): Int = if (b == 0) abs(a) else gcd(b, a % b)
    *    ...
    *  }
    * }}}
    */
+  class Fraction private(n: Int, d: Int) {
+
+    private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d)
+
+    private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d)
+
+    private def sign(a: Int) =
+      if (a > 0) 1
+      else if (a < 0) -1
+      else 0
+
+    @tailrec
+    private def gcd(a: Int, b: Int): Int =
+      if (b == 0) Math.abs(a)
+      else gcd(b, a % b)
+
+    def +(other: Fraction): Fraction = sumOp(other, _ + _)
+
+    def -(other: Fraction): Fraction = sumOp(other, _ - _)
+
+    def *(other: Fraction): Fraction = Fraction(num * other.num, den * other.den)
+
+    def /(other: Fraction): Fraction = Fraction(num * other.den, den * other.num)
+
+    private def sumOp(other: Fraction, op: (Int, Int) => Int): Fraction = {
+      val div = gcd(den, other.den)
+      Fraction(op(num * (div / den), other.num * (div / other.den)), div)
+    }
+
+    override def toString = num + "/" + den
+  }
+
+  object Fraction {
+    def apply(n: Int, d: Int) = new Fraction(n, d)
+  }
 }
--- src/test/scala/Chapter11Spec.scala
+++ src/test/scala/Chapter11Spec.scala
@@ -8,4 +8,13 @@ class Chapter11Spec extends FlatSpec with Matchers {
     (3 + 4 -> 5) shouldBe ((3 + 4) -> 5)
     (3 -> 4 + "5") shouldBe ((3 -> 4) + "5")
   }
+
+  "Fraction" should "be normalized and has operations +, -, *, /" in {
+    //when & then
+    Fraction(15, -6).toString shouldBe "-5/2"
+    (Fraction(1, 2) + Fraction(1, 2)).toString shouldBe "1/1"
+    (Fraction(1, 2) - Fraction(1, 2)).toString shouldBe "0/1"
+    (Fraction(1, 2) * Fraction(1, 2)).toString shouldBe "1/4"
+    (Fraction(1, 2) / Fraction(1, 2)).toString shouldBe "1/1"
+  }
 }
