810026ef1cd2937cb2a6fefefb46a9e9db28b595
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-03-27 11:47:46 +0200

    Added solution for task 15.2

--- src/main/scala/Chapter15.scala
+++ src/main/scala/Chapter15.scala
@@ -28,4 +28,31 @@ object Chapter15 {
       throw new RuntimeException()
     }
   }
+
+  /**
+   * Task 2:
+   *
+   * Make an example class that shows every possible position of an annotation.
+   * Use `@deprecated` as your sample annotation.
+   *
+   * Solution:
+   *
+   * I've used `@unchecked` instead to avoid compilation warnings, since `@deprecated` now takes
+   * two arguments.
+   */
+  @unchecked
+  class Deprecated[@unchecked T] @unchecked() (@unchecked val field: Int) {
+
+    @unchecked
+    type Dep = Int @unchecked
+
+    @unchecked
+    var field2: Int @unchecked = 0
+
+    @unchecked
+    def unchecked(@deprecatedName('oldArg) newArg: Int): Int = {
+      // annotation on an expression
+      1 + 2: @unchecked
+    }
+  }
 }
