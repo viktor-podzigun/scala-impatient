8289f94be535802537e77f16d8c4e70246adb5e7
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-04-13 10:43:29 +0200

    Added solution for task 17.4

--- src/main/scala/Chapter17.scala
+++ src/main/scala/Chapter17.scala
@@ -1,37 +1,72 @@
 
 
-object Chapter17 {
+/**
+ * Task 1:
+ *
+ * Define an immutable `class Pair[T, S]` with a method `swap` that returns a new pair with
+ * the components swapped.
+ */
+package task1701 {
 
-  /**
-   * Task 1:
-   *
-   * Define an immutable `class Pair[T, S]` with a method `swap` that returns a new pair with
-   * the components swapped.
-   */
-  case class Pair[T, S](first: T, second: S) {
+class Pair[T, S](val first: T, val second: S) {
 
-    def swap(): Pair[S, T] = Pair(second, first)
+  def swap(): Pair[S, T] = new Pair(second, first)
+}
+
+}
+
+/**
+ * Task 2:
+ *
+ * Define a mutable `class Pair[T]` with a method `swap` that swaps the components of the pair.
+ */
+package task1702 {
+
+class Pair[T](var first: T, var second: T) {
+
+  def swap(): Unit = {
+    val tmp = first
+    first = second
+    second = tmp
   }
+}
 
-  /**
-   * Task 2:
-   *
-   * Define a mutable `class Pair[T]` with a method `swap` that swaps the components of the pair.
-   */
-  class MutablePair[T](var first: T, var second: T) {
+}
 
-    def swap(): Unit = {
-      val tmp = first
-      first = second
-      second = tmp
-    }
-  }
+/**
+ * Task 3:
+ *
+ * Given a `class Pair[T, S]`, write a generic method `swap` that takes a pair as its argument
+ * and returns a new pair with the components swapped.
+ */
+object Chapter17Task03 {
 
-  /**
-   * Task 3:
-   *
-   * Given a `class Pair[T, S]`, write a generic method `swap` that takes a pair as its argument
-   * and returns a new pair with the components swapped.
-   */
-  def swap[T, S](pair: Pair[T, S]): Pair[S, T] = Pair(pair.second, pair.first)
+  import task1701._
+
+  def swap[T, S](pair: Pair[T, S]): Pair[S, T] = new Pair(pair.second, pair.first)
+
+}
+
+/**
+ * Task 4:
+ *
+ * Why don't we need a lower bound for the `replaceFirst` method in Section 17.3,
+ * "Bounds for Type Variables”, on page 232 if we want to replace the first component of
+ * a `Pair[Person]` with a `Student`?
+ *
+ * Solution:
+ *
+ * We don't need a lower bound because we replacing with a sub-class, which is OK, since
+ * the result type is still `Pair[Person]`.
+ */
+package task1704 {
+
+class Person(val name: String)
+class Student(name: String) extends Person(name)
+
+class Pair[T](val first: T, val second: T) {
+
+  def replaceFirst(newFirst: T): Pair[T] = new Pair(newFirst, second)
+}
+
 }
--- src/test/scala/Chapter17Spec.scala
+++ src/test/scala/Chapter17Spec.scala
@@ -1,11 +1,12 @@
-import Chapter17._
 import org.scalatest.{FlatSpec, Matchers}
 
 class Chapter17Spec extends FlatSpec with Matchers {
 
   "Pair.swap" should "return a new pair with the components swapped" in {
+    import task1701._
+
     //given
-    val pair: Pair[Int, String] = Pair(1, "2")
+    val pair: Pair[Int, String] = new Pair(1, "2")
 
     //when
     val result: Pair[String, Int] = pair.swap()
@@ -16,8 +17,10 @@ class Chapter17Spec extends FlatSpec with Matchers {
   }
 
   "MutablePair.swap" should "swap the components of the mutable pair" in {
+    import task1702._
+
     //given
-    val pair: MutablePair[Int] = new MutablePair(1, 2)
+    val pair: Pair[Int] = new Pair(1, 2)
 
     //when
     pair.swap()
@@ -27,15 +30,31 @@ class Chapter17Spec extends FlatSpec with Matchers {
     pair.second shouldBe 1
   }
 
-  "swap" should "take a pair and return a new pair with the components swapped" in {
+  "Task03.swap" should "take a pair and return a new pair with the components swapped" in {
+    import task1701._
+
     //given
-    val pair: Pair[Int, String] = Pair(1, "2")
+    val pair: Pair[Int, String] = new Pair(1, "2")
 
     //when
-    val result: Pair[String, Int] = swap(pair)
+    val result: Pair[String, Int] = Chapter17Task03.swap(pair)
 
     //then
     result.first shouldBe "2"
     result.second shouldBe 1
+  }
+
+  "Task04.replaceFirst" should "take a pair and return a new pair with the components swapped" in {
+    import task1704._
+
+    //given
+    val pair: Pair[Person] = new Pair[Person](new Person("First"), new Person("Second"))
+
+    //when
+    val result: Pair[Person] = pair.replaceFirst(new Student("newFirst"))
+
+    //then
+    result.first.name shouldBe "newFirst"
+    result.second.name shouldBe "Second"
   }
 }
