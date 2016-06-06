da5f67345165a73f7d814c12606e34ed37140989
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-06-06 09:59:02 +0200

    Added solution for task 21.1

--- src/main/scala/Chapter21.scala
+++ src/main/scala/Chapter21.scala
@@ -5,5 +5,12 @@ object Chapter21 {
   /**
    * Task 1:
    *
+   * How does `->` work? That is, how can `"Hello" -> 42` and `42 -> "Hello"` be pairs
+   * `("Hello", 42)` and `(42, "Hello")`? Hint: `Predef.any2ArrowAssoc`.
+   *
+   * Solution:
+   *
+   * Currently, as of Scala 2.11.x, its implemented by using `implicit class ArrowAssoc` that
+   * enriches any instance with `->` method.
    */
 }
