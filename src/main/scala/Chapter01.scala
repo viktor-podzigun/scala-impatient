1130be8a940ab5bba50ee7543108ac9ea441f235
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-06-30 11:00:04 +0200

    Added solution for task 1.1

--- src/main/scala/Chapter01.scala
+++ src/main/scala/Chapter01.scala
@@ -5,5 +5,23 @@ object Chapter01 {
   /**
    * Task 1:
    *
+   * In the Scala REPL, type `3.` followed by the Tab key. What methods can be applied?
+   *
+   * Solution:
+   *
+   * On my machine, when typing `3.` and pressing the Tab key for the first time,
+   * these methods are available:
+   * {{{
+   * %   &   *   +   -   /   >   >=   >>   >>>   ^   asInstanceOf   isInstanceOf
+   * toByte   toChar   toDouble   toFloat   toInt   toLong   toShort   toString
+   * unary_+   unary_-   unary_~   |
+   * }}}
+   * And when pressing the Tab key the second time:
+   * {{{
+   * !=   %   *   -   <    <=       ==   >=   >>>   asInstanceOf   getClass   isInstanceOf
+   * toChar     toFloat   toLong    toString   unary_-   |
+   * ##   &   +   /   <<   <init>   >    >>   ^     equals         hashCode
+   * toByte         toDouble   toInt     toShort   unary_+    unary_~
+   * }}}
    */
 }
