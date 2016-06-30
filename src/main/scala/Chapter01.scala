88b436e213805a5c20e20a0970aa41bd44c9127e
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-06-30 11:04:58 +0200

    Added solution for task 1.2

--- src/main/scala/Chapter01.scala
+++ src/main/scala/Chapter01.scala
@@ -24,4 +24,24 @@ object Chapter01 {
    * toByte         toDouble   toInt     toShort   unary_+    unary_~
    * }}}
    */
+
+  /**
+   * Task 2:
+   *
+   * In the Scala REPL, compute the square root of 3, and then square that value.
+   * By how much does the result differ from 3? (Hint: The res variables are your friend.)
+   *
+   * Solution:
+   *
+   * {{{
+   * scala> Math.sqrt(3)
+   * res0: Double = 1.7320508075688772
+   *
+   * scala> res0 * res0
+   * res1: Double = 2.9999999999999996
+   *
+   * scala> 3 - res1
+   * res2: Double = 4.440892098500626E-16
+   * }}}
+   */
 }
