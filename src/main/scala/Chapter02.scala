ed603f6ef1e5870bdd87cb98fafff98d47c955a7
Viktor Podzigun <viktor.podzigun@gmail.com>
2016-07-06 10:07:20 +0200

    Added solution for task 2.1

--- src/main/scala/Chapter02.scala
+++ src/main/scala/Chapter02.scala
@@ -5,5 +5,12 @@ object Chapter02 {
   /**
    * Task 1:
    *
+   * The `signum` of a number is `1` if the number is positive, `–1` if it is negative,
+   * and `0` if it is zero. Write a function that computes this value.
    */
+  def signum(n: Int): Int = {
+    if (n < 0) -1
+    else if (n > 0) 1
+    else 0
+  }
 }
--- src/test/scala/Chapter02Spec.scala
+++ src/test/scala/Chapter02Spec.scala
@@ -3,6 +3,12 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Chapter02Spec extends FlatSpec with Matchers {
 
-  "" should "" in {
+  "signum" should "return -1 if n < 0, 1 if n > 0 and 0 if n = 0" in {
+    //when & then
+    signum(-2) shouldBe -1
+    signum(-1) shouldBe -1
+    signum(0) shouldBe 0
+    signum(1) shouldBe 1
+    signum(2) shouldBe 1
   }
 }
